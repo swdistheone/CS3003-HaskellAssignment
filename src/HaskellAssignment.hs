3198350879283f3dd9a5aca0458dc692652a3b37
Sean W. Dorsey <164954483+swdistheone@users.noreply.github.com>
2025-08-08 03:16:31 -0400

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,5 +1,5 @@
 module HaskellAssignment where
-
+ import Data.List (group, sort)
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
@@ -8,8 +8,38 @@ instance Show Found where
   show (Match index) = "Found match at " ++ show index
   show NoMatch = "No match found!"
 findFirst :: Eq a => (a -> Bool) -> [a] -> Found
-
+findFirst p xs = go 0 xs
+  where
+    go _ [] = NoMatch
+    go n (x:xs)
+      | p x       = Match n
+      | otherwise = go (n + 1) xs
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
 palindrome :: [Char] -> Bool
+palindrome xs = xs == reverse xs
+------------------------------------------------  
+-- mergeSort
+------------------------------------------------  
+mergeSort :: (a -> a -> Bool) -> [a] -> [a]
+mergeSort _ [] = []
+mergeSort _ [x] = [x]
+mergeSort cmp xs = merge (mergeSort cmp left) (mergeSort cmp right)
+  where
+    (left, right) = splitAt (length xs `div` 2) xs
+    merge [] ys = ys
+    merge xs [] = xs
+    merge (x:xs) (y:ys)
+      | cmp x y   = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
+------------------------------------------------
+-- lengthEncode
+------------------------------------------------
+data RunLength = Span Int Char deriving Eq
+
+instance Show RunLength where
+  show (Span n c) = show n ++ " " ++ [c]
+
+lengthEncode :: [Char] -> [RunLength]
+lengthEncode = map (\grp -> Span (length grp) (head grp)) . group . sort
