7d7d81375f2320d5beedf2c6517d5650e7c6a59d
Sean W. Dorsey <164954483+swdistheone@users.noreply.github.com>
2025-08-08 04:03:23 -0400

    Update HaskellAssignment.hs

--- src/HaskellAssignment.hs
+++ src/HaskellAssignment.hs
@@ -1,55 +1,45 @@
 module HaskellAssignment where
-
-import Data.List (group)
-
+ import Data.List (group)
 ------------------------------------------------
 -- findFirst
 ------------------------------------------------
-data Found = Match Int | NoMatch
-  deriving Eq
-
+data Found = Match Int | NoMatch deriving Eq
 instance Show Found where
-  show (Match i) = "Found match at " ++ show i
-  show NoMatch   = "No match found!"
-
-findFirst :: (a -> Bool) -> [a] -> Found
+  show (Match index) = "Found match at " ++ show index
+  show NoMatch = "No match found!"
+findFirst :: Eq a => (a -> Bool) -> [a] -> Found
 findFirst p xs = go 0 xs
   where
-    go _ []     = NoMatch
-    go i (y:ys)
-      | p y       = Match i
-      | otherwise = go (i + 1) ys
-
+    go _ [] = NoMatch
+    go n (x:xs)
+      | p x       = Match n
+      | otherwise = go (n + 1) xs
 ------------------------------------------------
 -- palindrome
 ------------------------------------------------
-palindrome :: String -> Bool
-palindrome s = s == reverse s
-
-------------------------------------------------
+palindrome :: [Char] -> Bool
+palindrome xs = xs == reverse xs
+------------------------------------------------  
 -- mergesort
-------------------------------------------------
+------------------------------------------------  
 mergesort :: (a -> a -> Bool) -> [a] -> [a]
-mergesort _ []  = []
+mergesort _ [] = []
 mergesort _ [x] = [x]
-mergesort cmp xs =
-  let (l, r) = splitAt (length xs `div` 2) xs
-  in merge (mergesort cmp l) (mergesort cmp r)
+mergesort cmp xs = merge (mergesort cmp left) (mergesort cmp right)
   where
+    (left, right) = splitAt (length xs `div` 2) xs
     merge [] ys = ys
     merge xs [] = xs
-    merge xa@(x:xs') ya@(y:ys')
-      | cmp x y   = x : merge xs' ya
-      | otherwise = y : merge xa ys'
-
+    merge (x:xs) (y:ys)
+      | cmp x y   = x : merge xs (y:ys)
+      | otherwise = y : merge (x:xs) ys
 ------------------------------------------------
 -- runLengthEncode
 ------------------------------------------------
-data RunLength = Span Int Char
-  deriving Eq
+data RunLength = Span Int Char deriving (Eq, Show)
 
 instance Show RunLength where
-  show (Span n c) = "(Span " ++ show n ++ " " ++ show c ++ ")"
+  show (Span n c) = show n ++ " " ++ [c]
 
 runLengthEncode :: String -> [RunLength]
-runLengthEncode = map (\grp -> Span (length grp) (head grp)) . group
+runLengthEncode = map (\grp -> Span (length grp) (head grp)) . group 
